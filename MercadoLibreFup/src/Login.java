872a348c7bae14ec3dfa5293bf41feb65859f0fa
diego vidal <diego vidal@192.168.1.122>
2019-10-24 20:51:01 -0500

    camilo

--- MercadoLibreFup/src/Login.java
+++ MercadoLibreFup/src/Login.java
@@ -21,7 +21,7 @@ public static void main(String[] args)
     {
         System.out.println("Hola usuario bienvenido");
         System.out.println("A continuación se muestra una cuenta regresiva");
-
+System.out.println(" se muestra una cuenta regresiva");
         for (int i = 10; i >= 1;  i--)
         {
             System.out.println(i);//Se muestra cada valor de i (de 10 al 1)
